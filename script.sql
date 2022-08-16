b0665f81bd664b0a4fbaacf72e322ce374e79b1a
João Victor Corrêa <75680576+CorreaJo@users.noreply.github.com>
2022-08-16 10:26:04 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -18,5 +18,62 @@ create table if not exists loja_tenis.produto (
     imagem varchar(30) NULL DEFAULT NULL,
     categoria varchar(15) NULL DEFAULT NULL,
     preco decimal(9,2) NOT NULL,
-    primary key (idProduto)
+    idFornecedor INT NOT NULL,
+    primary key (idProduto),
+    CONSTRAINT fk_Fornecedor_Produto
+		FOREIGN KEY (idFornecedor)
+        REFERENCES loja_tenis.fornecedor (idFornecedor)
+);
+
+create table if not exists loja_tenis.usuario (
+	idUsuario INT NOT NULL auto_increment,
+    nome varchar(75) NOT NULL,
+    endereco varchar(150) NOT NULL,
+    telefone varchar(11) NULL DEFAULT NULL,
+    email varchar(150) NULL DEFAULT NULL,
+    cpf varchar(11) NOT NULL,
+    sexo varchar(1) NULL default NULL,
+    primary key (idUsuario)
+);
+
+create table if not exists loja_tenis.categoria (
+	idCategoria INT NOT NULL auto_increment,
+    nomeCategoria varchar(30) NOT NULL,
+    primary key (idCategoria)
+);
+
+create table if not exists loja_tenis.adm (
+	idAdm INT NOT NULL auto_increment,
+    nomeAdm varchar(75) NOT NULL,
+    emailAdm varchar(150) NOT NULL,
+    senhaAdm varchar (10) NOT NULL,
+    primary key (idAdm)
+);
+
+create table if not exists loja_tenis.gerente (
+	idGerente INT NOT NULL auto_increment,
+    nomeGerente varchar(75) NOT NULL,
+    emailGerente varchar(150) NOT NULL,
+    senhaGerente varchar (10) NOT NULL,
+    primary key (idGerente)
+);
+
+create table if not exists loja_tenis.fornecedor (
+	idFornecedor INT NOT NULL auto_increment,
+    nomeFornecedor varchar(75) NOT NULL,
+    CNPJ varchar(18) NOT NULL,
+    primary key (idFornecedor)
+);
+
+create table if not exists loja_tenis.pedido (
+	idPedido INT NOT NULL auto_increment,
+    idCategoria INT NOT NULL,
+    idProduto INT NOT NULL,
+    primary key (idPedido),
+    CONSTRAINT fk_Categoria_Pedido
+		FOREIGN KEY (idCategoria)
+        REFERENCES loja_tenis.categoria (idCategoria),
+	CONSTRAINT fk_Produto_Pedido
+		FOREIGN KEY (idProduto)
+        REFERENCES loja_tenis.produto (idProduto)
 );
