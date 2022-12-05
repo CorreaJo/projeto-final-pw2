f9bf91419be4fc87e3da10488ecbe3643b8b2dea
CorreaJo <j.victor.augusto3@gmail.com>
2022-12-04 21:43:55 -0300

    Arrumando Imagem

--- views/usuario/novoUsuario.php
+++ views/usuario/novoUsuario.php
@@ -26,12 +26,7 @@ if($imagem['name'] != ""){
    $to = $caminho;
    $from = $imagem["tmp_name"];
    
-   if(move_uploaded_file($from, $to)){
-      echo "Deu certo";
-   } else {
-      echo "nao deu";
-   }
-   die();
+   move_uploaded_file($from, $to);
 } else {
    $to = "../../../public/imagens/sem-foto.png";
    $from = $imagem["tmp_name"];
